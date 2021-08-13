d082bffd8b2d3b245239b613df5b0e86429c16b4
Chris Boren <chrisboren@gmail.com>
2021-08-13 09:00:16 -0700

    Initial Push from Personal Laptop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+// import './App.css';
 
 function App() {
+
+  const [baseFontSize, setBaseFontSize] = useState(16);
+
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--base-font', `${baseFontSize}px`);
+  }, [baseFontSize])
+
+  const increaseBase = () => {
+    // if (baseFontSize > 1) {
+    setBaseFontSize(baseFontSize + 1);
+    // }
+  }
+  const decreaseBase = () => {
+    if (baseFontSize > 1) {
+      setBaseFontSize(baseFontSize - 1);
+    }
+  }
+
+  const resetBase = () => {
+    setBaseFontSize(16);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="wrapper">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa earum expedita cumque laboriosam voluptatum? Placeat voluptas officiis molestias, repellendus, consectetur hic dolor nemo explicabo non iure, officia cupiditate vero reiciendis.</p>
+        </div>
+        <div className="wrapper">
+          <div className="buttons">
+            <p>Current base size is: {baseFontSize}</p>
+            <button onClick={increaseBase}>Increase</button>
+            <button onClick={decreaseBase}>Decrease</button>
+            <button onClick={resetBase}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
